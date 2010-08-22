7e8aa4b05561727135349c2ef7d813c700fe8217
Curtis (Programble) <programble@gmail.com>
2010-08-22 19:05:27 -0400

    Updated deps

--- project.clj
+++ project.clj
@@ -2,10 +2,11 @@
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [compojure "0.4.0"]
+                 [compojure "0.4.1"]
                  [ring/ring-jetty-adapter "0.2.3"]
                  [irclj "0.3.0-SNAPSHOT"]
                  [org.danlarkin/clojure-json "1.1"]
                  [clojure-http-client "1.1.0-SNAPSHOT"]
-                 [ring/ring-core "0.2.5"]]
+                 [ring/ring-core "0.2.5"]
+                 [hiccup "0.2.6"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
