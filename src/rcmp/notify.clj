8d012bebad40513a104583c44fb576e97879e9af
Curtis (Programble) <programble@gmail.com>
2010-09-10 22:12:35 -0400

    Or this

--- src/rcmp/notify.clj
+++ src/rcmp/notify.clj
@@ -12,11 +12,11 @@
 
 (defn format-commit [commit url?]
   (str (when url?
-         (str "<" (is-gd (:url commit)) "> "))
+         (str "<" (is-gd (:url commit)) "> \u0002"))
        (:name (:author commit))
-       ": \u0002"
+       "\u0002: "
        (apply str (take 8 (:id commit)))
-       " [\u0002"
+       " \u0002[\u0002"
        (->> [(map #(str "+" %) (:added commit))
              (map #(str "-" %) (:removed commit))
              (map #(str %) (:modified commit))]
