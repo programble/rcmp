3368d5ace5403aa42f557d2908971c284d973b2a
Curtis McEnroe <programble@gmail.com>
2011-03-14 15:46:16 -0400

    Added URL to main page. Closes #1

--- src/rcmp/web.clj
+++ src/rcmp/web.clj
@@ -9,6 +9,8 @@
          [:body
           [:h1 "RCMP"]
           [:p "RCMP is a Github commit notification IRC bot."]
+          [:h2 "Commit Hook URL"]
+          [:p "http://rcmp.programble.co.cc/github/<server>/<channel>"]
           [:h2 "Connected to"]
           [:ul
            (for [server (keys @irc-connections)]
