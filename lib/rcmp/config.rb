591a427a2ef010ea3aadbac522dd04432a5fe083
Curtis McEnroe <programble@gmail.com>
2013-10-12 16:56:09 -0400

    Add configuration loading

---
+++ lib/rcmp/config.rb
@@ -0,0 +1,19 @@
+require 'configru'
+
+Configru.load('rcmp.yml') do
+  option_group :irc do
+    option :nick, String, 'RCMP'
+
+    option_group :default do
+      option_required :server, String
+      option :port, Fixnum, 6667
+      option_required :channel, String
+    end
+
+    option_array :blacklist, String
+    option_array :whitelist, String
+
+    option :nicks, Hash, {}
+    option :nickserv, Hash, {}
+  end
+end
