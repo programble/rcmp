ba3374d14e51cdb4d27e4a0ea5949c4d8095b810
Curtis McEnroe <programble@gmail.com>
2013-10-13 20:04:12 -0400

    Initial GitHub Pages

---
+++ url.js
@@ -0,0 +1,42 @@
+$(function() {
+  function generateURL() {
+    var url = 'http://rcmp.pw/';
+    url += $('#server').val();
+    url += '/';
+    url += encodeURIComponent($('#channel').val());
+
+    var query = [];
+
+    if ($('#key').val())
+      query.push('key=' + encodeURIComponent($('#key').val()));
+
+    if (!$('#join').is(':checked'))
+      query.push('nojoin');
+
+    if ($('#part').is(':checked'))
+      query.push('part');
+
+    if ($('#notice').is(':checked'))
+      query.push('notice');
+
+    if (query.length)
+      url += '?' + query.join('&');
+
+    $('#url').val(url);
+  }
+
+  $('#server').change(generateURL);
+  $('#channel').keyup(generateURL);
+  $('#key').keyup(generateURL);
+  $('#join').change(generateURL);
+  $('#part').change(generateURL);
+  $('#notice').change(generateURL);
+
+  $('#url').focus(function() {
+    $(this).one('mouseup', function() {
+      return false; // Prevent deselect
+    }).select();
+  });
+
+  generateURL();
+});
